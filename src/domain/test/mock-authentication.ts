13c661394b76789921dae643ee86068a2d2ea7f8
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-09-28 16:48:37 -0300

    refactor: change imports to use module

--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,4 +1,4 @@
-import { AuthenticationParams } from 'domain/usecases/autentication'
+import { AuthenticationParams } from '@/domain/usecases/autentication'
 import faker from 'faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
