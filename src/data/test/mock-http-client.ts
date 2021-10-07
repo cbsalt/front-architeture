c6a1985d01389e5c854b45e55061bbb77be08bc1
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-10-07 16:21:40 -0300

    refactor: group imports in index files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,12 +1,16 @@
-import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
-import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse,
+  HttpStatusCode
+} from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
-  url?: string
-  body?: T
+  url?: string;
+  body?: T;
   response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok
-  }
+  };
 
   async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url
