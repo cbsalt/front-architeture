8a663ad4021d3bc9bed1f4442303676fffce3615
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-10-11 16:57:35 -0300

    test: ensure Login will not render spinner and error on start

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Login from './login'
+
+describe('Login Component', () => {
+  test('Should not render spinner and error on start', () => {
+    const { getByTestId } = render(<Login />)
+    const errorWrapper = getByTestId('error-wrapper')
+    expect(errorWrapper.childElementCount).toBe(0)
+  })
+})
