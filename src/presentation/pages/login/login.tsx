2f95d947a42c59150825e4d68318d82bd505e084
Charles Sal <charlessal@Charless-MacBook-Pro.local>
2021-10-11 15:47:52 -0300

    refactor: move input to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Spinner from '@/presentation/components/spinner/spinner'
 import Header from '@/presentation/components/login-header/login-header'
 import Footer from '@/presentation/components/footer/footer'
+import Input from '@/presentation/components/input/input'
 import Styles from './login-styles.scss'
 
 const Login: React.FC = () => {
@@ -13,30 +14,15 @@ const Login: React.FC = () => {
 
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrapper}>
-          <input type="email" name="email" placeholder="Digite seu e-mail" />
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrapper}>
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-          />
-          <span className={Styles.status}>🔴</span>
-        </div>
-
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
         <button className={Styles.submit} type="submit">
           Entrar
         </button>
-        <span className={Styles.link}>
-          Criar conta
-        </span>
+        <span className={Styles.link}>Criar conta</span>
         <div className={Styles.errorWrapper}>
           <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>
-            Erro
-          </span>
+          <span className={Styles.error}>Erro</span>
         </div>
       </form>
 
